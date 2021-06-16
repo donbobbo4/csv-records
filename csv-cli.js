f9d283de0a9da8eba98d4e06acb4d1ad66a74782
Bob Hill <robert.b.hill@gmail.com>
2021-06-16 17:43:33 -0400

    multiple file types

--- csv-cli.js
+++ csv-cli.js
@@ -8,18 +8,28 @@ const csv = require('@fast-csv/parse');
 const options = yargs
  .usage("Usage: -f <filename>")
  .option("f", { alias: "filename", describe: "filename to parse", type: "string", demandOption: true})
+ .option("t", { alias: "fileType", describe: "comma,pipe, or space", type: "string", demandOption: true})
  .argv;
 
-const greeting = `Hello, ${options.name}!`;
 const filename = options.filename;
+let fileType;
+switch(options.fileType) {
+    case "comma":
+        fileType = ","
+        break;
+    case "pipe":
+        fileType = "|"
+        break;
+    case "space":
+        fileType = " "
+        break;
+}
 
 console.log(filename);
 
-
-
 let parserFcn = new Promise((resolve, reject) => {
     const parser = csv
-        .parseFile(filename, { headers: true })
+        .parseFile(filename, { headers: true, delimiter: fileType })
         .on("data", function (data) {
             console.log(data);
         })
