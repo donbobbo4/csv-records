8019708b01a3f58a776b0ffa8388cfa4f2ea03ab
Bob Hill <robert.b.hill@gmail.com>
2021-06-16 19:05:09 -0400

    triple action sort

--- csv-cli.js
+++ csv-cli.js
@@ -28,16 +28,57 @@ switch(options.fileType) {
 console.log(filename);
 
 let parserFcn = new Promise((resolve, reject) => {
-    const parser = csv
-        .parseFile(filename, { headers: true, delimiter: fileType })
-        .on("data", function (data) {
-            console.log(data);
-        })
-        .on("end", function () {
-            resolve(true);
-        })
-        .on("error", function () {
-            reject(false);
-            console.log("++csv is in ERROR++")
-        });
+    let dataArr = [];
+    try{
+        const parser = csv
+            .parseFile(filename, { headers: true, delimiter: fileType })
+            .on("data", function (data) {
+                console.log(data);
+                dataArr.push(data);
+                
+            })
+            .on("end", function () {
+                resolve(true);
+                console.log("++++++++++++Favorite color and Lastname SORT ASC+++++++++++");
+                dataArr = dataArr.sort(sortByPropertyASC("FavoriteColor"));
+                console.log(dataArr.sort(sortByPropertyASC("LastName")));
+                
+                console.log("++++++++++++Birthdate SORT asc+++++++++++");
+                console.log(dataArr.sort((a,b) => new Date(a.DateOfBirth) - new Date(b.DateOfBirth)));
+
+                console.log("++++++++++++Lastname SORT DESC+++++++++++");
+                console.log(dataArr.sort(sortByPropertyDESC("LastName")));
+
+            })
+            .on("error", function () {
+                reject(false);
+                console.log("++csv is in ERROR++")
+            });
+
+        } catch(e){
+            console.log(e.stack)
+        }
     });
+
+    
+    function sortByPropertyASC(property){
+        return function(a,b){  
+            if(a[property] > b[property])  
+               return 1;  
+            else if(a[property] < b[property])  
+               return -1;  
+        
+            return 0;  
+         }  
+      }
+
+      function sortByPropertyDESC(property){
+        return function(a,b){  
+            if(a[property] > b[property])  
+               return -1;  
+            else if(a[property] < b[property])  
+               return 1;  
+        
+            return 0;  
+         }  
+      }
